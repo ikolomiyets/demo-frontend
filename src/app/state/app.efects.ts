bce31aa70452af111bbb50a87de9fda50a60c9e3
Igor Kolomiyets <ikolomiyets@gmail.com>
2023-08-27 15:42:12 +0100

    Upgrading to Angular 16

--- src/app/state/app.efects.ts
+++ src/app/state/app.efects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { HttpClient } from '@angular/common/http';
 import * as fromAppActions from './app.actions';
 import { Insured, Policies } from '../model/policy.model';
@@ -8,8 +8,7 @@ import aesjs from 'aes-js';
 
 @Injectable()
 export class AppEfects {
-  @Effect()
-  doLoadPolicies = this.actions$
+  doLoadPolicies = createEffect(() => this.actions$
     .pipe(ofType(fromAppActions.DO_LOAD_POLICIES))
     .pipe(map((action: fromAppActions.DoLoadPolicies) => action.payload),
     switchMap((payload: {baseUrl: string, first: number, count: number}) => {
@@ -20,10 +19,9 @@ export class AppEfects {
         type: fromAppActions.POLICIES_RECEIVED,
         payload: policies
       };
-    }));
+    })));
 
-  @Effect()
-  doInitCustomerRetrieval = this.actions$
+  doInitCustomerRetrieval = createEffect(() => this.actions$
     .pipe(ofType(fromAppActions.DO_INIT_CUSTOMER_RETRIEVAL),
       map((action: fromAppActions.DoInitCustomerRetrieval) => action.payload),
       flatMap((payload: {baseUrl: string, ids: number[]}) => {
@@ -33,10 +31,9 @@ export class AppEfects {
       }
 
       return result;
-    }));
+    })));
 
-  @Effect()
-  doRetrieveCustomer = this.actions$
+  doRetrieveCustomer = createEffect(() => this.actions$
     .pipe(ofType(fromAppActions.DO_RETRIEVE_CUSTOMER), map((action: fromAppActions.DoRetrieveCustomer) => action.payload),
     mergeMap((payload: { baseUrl: string, id: number }) => {
       return this.http.get<Insured>(`${payload.baseUrl}/customers/${payload.id}`);
@@ -47,12 +44,11 @@ export class AppEfects {
         type: fromAppActions.CUSTOMER_RECEIVED,
         payload: insured
       };
-    }));
+    })));
 
-  @Effect({dispatch: true})
-  doInit = this.actions$.pipe(ofType(fromAppActions.DO_INIT),
+  doInit = createEffect(() => this.actions$.pipe(ofType(fromAppActions.DO_INIT),
     switchMap((action: fromAppActions.DoInitAction) => {
-    return this.http.get<{ param: string; header: string; token: string}>('/assets/_etc/config.json')
+    return this.http.get<{ param: string; header: string; token: string}>('/assets/_etc/config.json');
   }), switchMap((result: any) => {
     const a = this.decryptConfig(result);
     return [{
@@ -69,8 +65,8 @@ export class AppEfects {
           count: 10,
         }
       }
-    ]
-  }));
+    ];
+  })), { dispatch: true });
 
   base64toBytes = (base64: string) => {
     var raw = atob(base64);
@@ -83,7 +79,7 @@ export class AppEfects {
 
   decryptConfig = (config: any) => {
     const size = 16;
-    const _abcdefg = this.base64toBytes("6tY6JQqfrWTxS2rcWgBXCLLzHDzBpZb3wk5Qb8DP+aM=");
+    const _abcdefg = this.base64toBytes('6tY6JQqfrWTxS2rcWgBXCLLzHDzBpZb3wk5Qb8DP+aM=');
     const payload = this.base64toBytes(config._KFbEEbp);
     const iv = payload.slice(0, size);
     const encrypted = payload.slice(size);
