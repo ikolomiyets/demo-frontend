bf0474b57047a647fa33c55d11e231219cfa14c1
Igor Kolomiyets <ikolomiyets@gmail.com>
2023-08-27 16:18:14 +0100

    Completed main migration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 import * as fromApp from './state/app.reducers';
 import * as fromAppAction from './state/app.actions';
@@ -17,17 +17,23 @@ export class AppComponent implements OnInit {
   customersBaseUrl: string;
   insured: number[];
   policyNumber: string;
+  @ViewChild('myModal') public myModal: ElementRef;
 
-  constructor(private store: Store<{app: fromApp.AppState}>, private modalService: NgbModal) {}
+  constructor(private store: Store<{app: fromApp.AppState}>, private modalService: NgbModal, private renderer: Renderer2) {}
   ngOnInit(): void {
     this.state = this.store.select('app');
     this.store.dispatch(new fromAppAction.DoInitAction());
   }
 
+
   async showInsured(content, policyNumber, insured: number[]) {
     this.store.dispatch(new fromAppAction.DoResetInsured());
     this.policyNumber = policyNumber;
-    this.store.dispatch(new fromAppAction.DoInitCustomerRetrieval({baseUrl: await this.state.pipe(select(state => state.config.DEMO_FRONTEND_CUSTOMERS_URL), take(1)).toPromise()
+    this.store.dispatch(
+      new fromAppAction.DoInitCustomerRetrieval(
+        {
+          baseUrl: await this.state.pipe(select(state => state.config.DEMO_FRONTEND_CUSTOMERS_URL), take(1))
+            .toPromise()
       , ids: insured}));
 
     this.modalService.open(content);
